41c18ff3890adc02956a508c2e8390f19972d6f7
CthulhuSpecimen <maxxm03@gmail.com>
2024-10-06 13:53:26 -0500

    The Ambient Update
    - Added Music/Ambience to Forest Proto
    - Added Music to Network Proto
    - Added small light to bullet
    - Added moving clouds to Forest Proto
    - Bullet now collides with tilemap
    - Gun Flip Fixed

--- putr-worm-game/Scripts/bullet.gd
+++ putr-worm-game/Scripts/bullet.gd
@@ -5,10 +5,14 @@ var direction = Vector2.ZERO
 
 func _ready():
 	set_process(true)  # Enable the process function
+	connect("body_entered", Callable(self, "_on_Area2D_body_entered"))  # Use a Callable for the connection
 
 func set_direction(target_position: Vector2):
 	direction = (target_position).normalized()  # Use the direction set from the player
 
 func _process(delta):
 	global_position += direction * speed * delta  # Move in the direction set
-	
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("tilemap"):  # Check if the colliding body is in the group 'tilemap'
+		queue_free()  # Free this node when collision occurs with a 'tilemap'
