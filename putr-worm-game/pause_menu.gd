5de9e5f470615fe37b7f1b191dc471c51f5a7144
ShahDevarsh0209 <145064591+ShahDevarsh0209@users.noreply.github.com>
2024-12-08 16:19:03 -0600

    Pause menu added

---
+++ putr-worm-game/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var checkpoint_button = $HBoxContainer/Respawn
+@onready var resume_button = $HBoxContainer/Resume
+@onready var exit_button = $HBoxContainer/Quit
+
+func _ready():
+	hide()
+	process_mode = Node.PROCESS_MODE_ALWAYS
+   
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		toggle_pause()
+
+func toggle_pause():
+	visible = !visible
+	get_tree().paused = visible
+
+func _on_respawn_pressed():
+	var player = get_tree().get_first_node_in_group("players")
+	
+	if player:
+		player.respawn() 
+		toggle_pause()
+
+func _on_resume_pressed():
+	print("Resume pressed")
+	toggle_pause()
+	
+
+func _on_exit_pressed():
+	get_tree().quit() 
